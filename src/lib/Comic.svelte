d49a9613134dfad11561be2deeea7b6868742cd1
BogGoro <151636316+BogGoro@users.noreply.github.com>
2024-07-03 16:35:37 +0300

    Homework 4

--- src/lib/Comic.svelte
+++ src/lib/Comic.svelte
@@ -2,42 +2,46 @@
   import { formatDistanceToNow } from "date-fns";
   import type { Comic } from "../interfaces/comic"
 
-var comicTitle = '' as string;
-var image = '' as string;
-var imageDesc = '' as string;
-var comicDate = '' as string;
-var comicFromNow = '' as string;
+  var comicTitle = '' as string;
+  var image = '' as string;
+  var imageDesc = '' as string;
+  var comicDate = '' as string;
+  var comicFromNow = '' as string;
 
-async function fetchComic() {
-    const params = new URLSearchParams() as URLSearchParams;
-    params.append('email', "d.troegubov@innopolis.university");
-    const comic_id = await fetch('https://fwd.innopolis.university/api/hw2?' + params.toString())
-        .then(r => r.json()) as string;
-    params.delete('email');
-    params.append('id', comic_id);
-    const comicObj = await fetch('https://fwd.innopolis.university/api/comic?' + params.toString())
-    .then(r => r.json()) as Comic;
-    return comicObj;
-}
+  async function fetchComic() {
+      const params = new URLSearchParams() as URLSearchParams;
+      params.append('email', "d.troegubov@innopolis.university");
+      const comic_id = await fetch('https://fwd.innopolis.university/api/hw2?' + params.toString())
+          .then(r => r.json()) as string;
+      params.delete('email');
+      params.append('id', comic_id);
+      const comicObj = await fetch('https://fwd.innopolis.university/api/comic?' + params.toString())
+      .then(r => r.json()) as Comic;
+      return comicObj;
+  }
 
-function handleCoimic(comicObj: Comic) {
-    const { safe_title } = comicObj;
-    comicTitle = safe_title;
+  function handleCoimic(comicObj: Comic) {
+      const { safe_title } = comicObj;
+      comicTitle = safe_title;
 
-    const { alt, img } = comicObj;
-    imageDesc = alt;
-    image = img;
+      const { alt, img } = comicObj;
+      imageDesc = alt;
+      image = img;
 
-    const { day, month, year } = comicObj;
-    const date = new Date(year, month, day) as Date;
-    comicDate = "Release date: " + date.toLocaleDateString();
-    comicFromNow = "Time from release: " + formatDistanceToNow(date);
-}
+      const { day, month, year } = comicObj;
+      const date = new Date(year, month, day) as Date;
+      comicDate = "Release date: " + date.toLocaleDateString();
+      comicFromNow = "Time from release: " + formatDistanceToNow(date);
+  }
 
-async function getComic() {
-    comicTitle = "Loading...";
-    handleCoimic(await fetchComic());
-};
+  async function getComic() {
+      comicTitle = "Loading...";
+      var image = '';
+      var imageDesc = '';
+      var comicDate = '';
+      var comicFromNow = '';
+      handleCoimic(await fetchComic());
+  };
 </script>
 
 <div class="main_block">
